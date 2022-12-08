63e29076c0b6f58ac0b969a937e1c0d0fd78d9c5
shch989 <tankpop11@gmail.com>
2022-12-08 22:00:18 +0900

    유저 엔티티 설정

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,16 +1,34 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { IsEmail } from 'class-validator'
+import { Length } from 'class-validator/types/decorator/decorators'
+import { Entity, PrimaryGeneratedColumn, Column, Index, OneToMany, BeforeInsert } from 'typeorm'
+import bcrypt from 'bcryptjs'
 
-@Entity()
+@Entity('users')
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number
+  @Index()
+  @IsEmail(undefined, { message: '이메일 주소가 잘못되었습니다.' })
+  @Length(1, 255, { message: '이메일 주소는 비워둘 수 없습니다.' })
+  @Column({ unique: true })
+  email: string
+
+  @Index()
+  @Length(3, 32, {message: "사용자 이름은 3자 이상이어야 합니다."})
+  @Column()
+  userName: string
 
   @Column()
-  firstName: string
+  @Length(6, 255, {message: "비밀번호는 6자리 이상이어야 합니다."})
+  password: string
 
-  @Column()
-  lastName: string
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[]
 
-  @Column()
-  age: number
+  @OneToMany(() => Vote, (vote) => vote.user)
+  votes: Vote[]
+
+  // 비밀번호는 DB에 저장되기 전에 Bcrypt로 Hash한다
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6)
+  }
 }
